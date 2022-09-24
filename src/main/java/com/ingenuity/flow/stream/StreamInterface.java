6019da1eaffe9a60d20b8bb04c63e3a0a6d25d23
yoganayagam <yoganayagam.edu@gmail.com>
2022-09-23 21:06:12 -0700

    refactored the function input variable

--- src/main/java/com/ingenuity/flow/stream/StreamInterface.java
+++ src/main/java/com/ingenuity/flow/stream/StreamInterface.java
@@ -12,12 +12,12 @@ public interface StreamInterface {
 
     public TransformGraph getTransformGraph();
 
-    public void setTransformQueue(int transformID, BlockingQueue<?> bQueue);
+    public void setTransformQueue(int tHashcode, BlockingQueue<?>[] bQueue);
 
-    public BlockingQueue<?> getTransformQueue(int transformID);
+    public BlockingQueue<?> [] getTransformQueue(int tHashcode);
 
-    public HashMap<Integer, BlockingQueue<?>> getUpstreamQueue(int transformID);
+    public HashMap<Integer, BlockingQueue<?>[]> getUpstreamQueue(int tHashcode);
 
-    public HashMap<Integer, BlockingQueue<?>> getDownstreamQueue(int transformID);
+    public HashMap<Integer, BlockingQueue<?>[]> getDownstreamQueue(int tHashcode);
 
 }
