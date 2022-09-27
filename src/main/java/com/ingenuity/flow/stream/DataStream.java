ec4ef558d2ef712dea8f802059ec77097808692a
yoganayagam <yoganayagam.edu@gmail.com>
2022-09-27 14:33:49 -0700

    Included DataStream java class, first concrete implementation of AbstractStream class

---
+++ src/main/java/com/ingenuity/flow/stream/DataStream.java
@@ -0,0 +1,12 @@
+package com.ingenuity.flow.stream;
+
+import com.ingenuity.flow.struct.TransformGraph;
+
+public class DataStream extends AbstractStream {
+
+    public DataStream(TransformGraph tGraph){
+        super();
+        this.setTransformGraph(tGraph);
+
+    }
+}
