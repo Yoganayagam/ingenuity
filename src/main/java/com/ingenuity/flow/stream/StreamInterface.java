49b003979e13be51e2ef50971d88680d6e6b239e
yoganayagam <yoganayagam.edu@gmail.com>
2022-09-27 14:31:28 -0700

    included functions: setUpstreamQueues() , setDownstreamQueues().

--- src/main/java/com/ingenuity/flow/stream/StreamInterface.java
+++ src/main/java/com/ingenuity/flow/stream/StreamInterface.java
@@ -2,6 +2,8 @@ package com.ingenuity.flow.stream;
 
 import com.ingenuity.flow.struct.TransformGraph;
 import com.ingenuity.transform.Transform;
+import com.ingenuity.transform.TransformLink;
+import com.ingenuity.transform.TransformLinkSet;
 
 import java.util.HashMap;
 import java.util.concurrent.BlockingQueue;
@@ -19,5 +21,9 @@ public interface StreamInterface {
     public HashMap<Integer, BlockingQueue<?>[]> getUpstreamQueue(int tHashcode);
 
     public HashMap<Integer, BlockingQueue<?>[]> getDownstreamQueue(int tHashcode);
+
+    public boolean setUpstreamQueues(int tHashcode, TransformLinkSet<TransformLink> linkSet);
+
+    public boolean setDownstreamQueues(int tHashcode, TransformLinkSet<TransformLink> linkSet);
 
 }
